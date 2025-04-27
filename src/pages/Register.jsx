b00e03c9c07d93e11362c51f76bb97f3c37286e4
GEORGIOS MOYSIADIS <georgemois23@gmail.com>
2025-04-27 19:17:10 +0300

    f

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -12,7 +12,7 @@ const Register = () => {
                 Εδώ θα βρεις τις φόρμες συμμετοχής στην ομάδα!
                 </Text>
               <Flex direction={'column'} width={'fit-content'} alignItems={'center'} borderRadius={8} gap={4} bg='rgba(0, 10, 38, 0.6)' backdropFilter='blur(4px)' boxShadow="0 8px 32px rgba(0, 0, 0, 0.3)"
-                border="1px solid rgba(251, 125, 30, 0.18)"  px={{ sm: 21, lg: 20 }} marginInline={'auto'}>
+                border="1px solid rgba(255, 255, 255, 0.05)"  px={{ sm: 21, lg: 20 }} marginInline={'auto'}>
                 <Flex direction={'column'} width={'fit-content'} alignItems={'center'}  gap={2}  padding={4} py={{sm:0,lg:5}}  px={{ sm: 21, lg: 20 }} marginInline={'auto'}>
                 <Text fontSize={{ base: 'lg', lg: 'xl' }} fontWeight="bold" mb={2}>
                     Εγγραφή στην ομάδα Open Source UoM</Text>                    
